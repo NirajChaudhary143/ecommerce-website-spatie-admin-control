cf6222b1feaa2c87d39c375987170d463d50c79b
NirajChaudhary143 <nirajchy2000@gmail.com>
2023-06-13 16:08:16 +0545

    spatie-role-set

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // password
+        ]);
+
+        $adminRole = Role::findByName('admin');
+        $admin->assignRole($adminRole);
+
+        $staff = User::create([
+            'name' => 'staff',
+            'email' => 'staff@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // password
+        ]);
+
+        $staffRole = Role::findByName('staff');
+        $staff->assignRole($staffRole);
+
+        $user =User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // password
+        ]);
+        $userRole = Role::findByName('user');
+        $user->assignRole($userRole);
+
+    }
+}
